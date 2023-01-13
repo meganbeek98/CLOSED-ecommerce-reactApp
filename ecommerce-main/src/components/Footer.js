ca421d6120d3e08c208056cd9e220ff9bc2c434e
meganbeek98 <meganbeek98@gmail.com>
2023-01-13 18:31:42 -0500

    initial commit

---
+++ ecommerce-main/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App.css'
+const Footer = () => {
+  return (
+    <div classname="footer">
+        <p>Ecommerce App 2022&copy;</p>
+    </div>
+  )
+}
+
+export default Footer
